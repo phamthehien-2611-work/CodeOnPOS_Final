a47d7cecc7fac0ab535dec12488e8060d0d941d3
Phạm Thế Hiển <phamthehien.2611.work@gmail.com>
2025-06-18 23:40:55 +0700

    no message

---
+++ Back/services/ProductManagementService/ProductManagementService.Api/Services/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using Grpc.Core;
+using Shared.GrpcContracts.Product;
+
+namespace ProductManagementService.Api.Services
+{
+    public class ProductService : ProductConfigService.ProductConfigServiceBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
---
+++ Back/services/UserManagementService/UserManagementService.Api/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using Grpc.Core;
+using Shared.GrpcContracts.User;
+
+namespace UserManagementService.Api.Services
+{
+    public class UserService : UserConfigService.UserConfigServiceBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
